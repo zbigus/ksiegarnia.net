b29f23b6c12590d95f424fb1d4136e5f4f3d6794
lachmateuszb <lachmateuszb@gmail.com>
2014-05-23 22:44:22 +0200

    updated version

---
+++ BookStore.Logic/Models/ModelFactory.cs
@@ -0,0 +1,40 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using BookStore.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Logic.Models
+{
+    public class ModelFactory
+    {
+        public ModelFactory(){
+        }
+
+        public UserModel Create(User user)
+        {
+            return new UserModel
+            {
+                ID = user.ID,
+                Login = user.Login,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Email = user.Email,
+                Address = user.Address,
+                RoleID = user.RoleID
+            };
+        }
+
+        public SampleBookModel CreateInitial(Book b)
+        {
+            return new SampleBookModel
+            {
+                Author = b.Author,
+                Title =  b.Title,
+                Price = b.Price
+            };
+        }
+    }
+}
---
+++ BookStore_SPA/Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+﻿using System.Data.Entity;
+using BookStore.Logic.Models;
+using BookStore.Logic.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BookStore_SPA.Controllers
+{
+    public class UsersController : BaseApiController
+    {
+        public UsersController(IRepository repo) : base(repo) { }
+
+        public IHttpActionResult Get() {
+            var query = Repo.GetAllUsers();
+            var results = query.ToList();
+            return Ok(results);
+        }
+        [Route("api/Users/{role}")]
+        public IHttpActionResult Get(string role) {
+            var users = Repo.GetUsersByRole(role);
+            if (users == null)
+            {
+                return NotFound();
+            }
+            var result = users.ToList().Select(s => TheModelFactory.Create(s));
+            return Ok(result);
+        }
+    }
+}
