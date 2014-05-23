22fc4e261cfde07464887f98bded80a4d4df9b58
haskisc <haskisc@gmail.com>
2014-05-23 09:27:00 +0200

    Poprawa wczorajszej wbitki

---
+++ BookStore.Entities/Models/BookCategory.cs
@@ -0,0 +1,11 @@
+﻿namespace BookStore.Entities.Models
+{
+    public class BookCategory
+    {
+        public int BookID { get; set; }
+        public int CategoryID { get; set; }
+
+        public virtual Book Book { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
