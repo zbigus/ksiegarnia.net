f149c3b7cbd3b07fc3a8a849fe9d33f1fd6b2db4
haskisc <haskisc@gmail.com>
2014-06-08 22:24:00 +0200

    Rozszerzenie MeControllera

--- BookStore.Logic/Models/MeViewModels.cs
+++ BookStore.Logic/Models/MeViewModels.cs
@@ -4,5 +4,9 @@
     public class GetViewModel
     {
         public string UserName { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
     }
 }
