f20c0e277243a2c98345182668c3889846f13d40
haskisc <haskisc@gmail.com>
2014-06-09 20:09:48 +0200

    Dla Radziunia niunia niunia

--- BookStore_SPA/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
+++
@@ -1,9 +0,0 @@
-namespace BookStore.SPA.Areas.HelpPage.ModelDescriptions
-{
-    public class KeyValuePairModelDescription : ModelDescription
-    {
-        public ModelDescription KeyModelDescription { get; set; }
-
-        public ModelDescription ValueModelDescription { get; set; }
-    }
-}
